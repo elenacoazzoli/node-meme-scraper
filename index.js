136b267e03c4a0fd79badc446e272c7623070c3a
elenacoazzoli <coazzoli.elena@gmail.com>
2021-09-09 15:25:11 +0200

    Adds download functionality and new dependencies.

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
+import fs from 'node:fs';
 // Loading the dependencies.
-const axios = require('axios');
-const cheerio = require('cheerio');
+import axios from 'axios';
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
 
 // URL of the page we want to scrape the memes from
 const memeUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
+
+async function download(url, idx) {
+  const response = await fetch(url);
+  const buffer = await response.buffer();
+  fs.writeFile(`./memes/image${idx}.jpg`, buffer, () =>
+    console.log('finished downloading: ' + url),
+  );
+}
 
 // await is valid in async functions
 async function scrapeData() {
   // Fetch HTML of the page using axios
   const { data } = await axios.get(memeUrl);
-  // console.log(data);
   // Load HTML we fetched using cheerio
   const htmlDataLoaded = cheerio.load(data);
   // Select all the images
   const imgSelection = htmlDataLoaded('#images div a img');
-  // console.log(imgSelection);
-  // Stores data for all images
-  const images = [];
-  // console.log(images);
-  // Use .each method to loop through the images we selected
+  // Use .each jQuery method to loop through the images we selected
   imgSelection.each((idx, el) => {
     if (idx < 10) {
-      // console.log(htmlDataLoaded(el).attr('src'));
-      // Object holding url for each meme
-      const meme = { memeLink: '' };
-      // Select the url content of the src in the img elements
-      // Store the url in the above object
-      meme.memeLink = htmlDataLoaded(el).attr('src');
-      // Populate image array with the meme data
-      images.push(meme);
-      console.log(images);
+      // use download function and the url is passed through .attr method
+      download(htmlDataLoaded(el).attr('src'), idx);
+    } else {
+      // else condition in order to break the loop, when the first 10 images are stored
+      return false;
     }
   });
 }
+
 scrapeData();
